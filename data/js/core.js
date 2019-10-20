a8eb3e189d03abdcdb5adddffd1b5a9e87fe5f86
Sascha K <ku3menko.sasha@gmail.com>
2019-10-20 23:22:44 +0200

    WIP: Pages

--- data/js/core.js
+++ data/js/core.js
@@ -1,4 +1,12 @@
 const body = document.querySelector('body');
+let storiesCache;
+let commentsCache;
+let storiesPerPage = 10;
+let page = 1;
+
+const baseUrl = "https://hacker-news.firebaseio.com/v0/";
+const topStoriesUrl = baseUrl + "topstories.json";
+// const container = document.querySelector('container');
 body.addEventListener('click', handleClick);
 
 function handleClick(e) {
@@ -6,14 +14,18 @@ function handleClick(e) {
         body.innerHTML = '';
         //console.log(e.target.parentNode.id);
         openStory(e.target.parentNode.id);   
-    } else if (e.target.classList.contains('story')) {    
+    } else if (e.target.classList.contains('button_previous')) {
+        if (page > 1) {
+            page--;
+            loadPage(page, storiesPerPage);
+        }
+    } else if (e.target.classList.contains('button_next')) {
+        page++;
+        loadPage(page, storiesPerPage);
     }
 }
 
 // https://hacker-news.firebaseio.com/v0/item/21278545.json
-
-
-const baseUrl = "https://hacker-news.firebaseio.com/v0/";
 // const topStoriesUrl = baseUrl + "topstories.json"
 //https://hacker-news.firebaseio.com/v0/user/ryanlol.json
 
@@ -52,28 +64,22 @@ function loadComments(commentIDs, appendTo = body, depth = 0, count = 10) {
 
 function openStory(storyID) {
     loadStories([storyID]);
-    loadCommentsIDs(storyID).then(pr => console.log(pr));
+    loadCommentsIDs(storyID);
     loadCommentsIDs(storyID).then(pr => loadComments(pr, body));
 }
 
-let storiesCache;
-let commentsCache;
-
-const topStoriesUrl = baseUrl + "topstories.json";
-
-function getTopStoriesIDs(url, from = 0, to = 10) {
+function getTopStoriesIDs(url) {
     return fetch(url).then(response => {
         if (!response.ok) {
             throw Error (response.statusText);
         }
         return response.json();
-    }).then(storiesJson => {
-        storiesCache = storiesJson.slice(from, to);
-        return storiesCache;
     });
 }
 
 function loadStories(storyIDArr) {
+    let promisesArr = [];
+    //let limit = (storyIDArr.length < count) ? storyIDArr.length : count;
     for (let i = 0; i < storyIDArr.length; i++) {
         let url = baseUrl + "item/" + storyIDArr[i] + ".json";
         const div = document.createElement('div');
@@ -82,23 +88,47 @@ function loadStories(storyIDArr) {
         div.classList.add('story');
         p.classList.add('more_comments');
         p.innerText = "comments";
-        fetch(url).then(storyJson => {
-                return storyJson.json();
-        }).then(storyJson => {
-                console.log(storyIDArr[i]);
-                div.id = storyJson.id;
-                link.href = storyJson.url;
-                link.innerText = storyJson.title;
-                commentsCache = storyJson.kids;
-                div.appendChild(link);
-                div.appendChild(p);
-                body.appendChild(div);
-        });
+        promisesArr.push(
+            fetch(url).then(storyJson => {
+                    return storyJson.json();
+            }).then(storyJson => {
+                    div.id = storyJson.id;
+                    link.href = storyJson.url;
+                    link.innerText = storyJson.title;
+                    commentsCache = storyJson.kids;
+                    div.appendChild(link);
+                    div.appendChild(p);
+                    body.appendChild(div);
+            })
+        );
     }
+    return Promise.all(promisesArr);
 }
+// 0-9, 10-19, 20-29
+//0-14, 15-29, 30-45
 
-getTopStoriesIDs(topStoriesUrl).then(storyIDArr => {
-    loadStories(storyIDArr);
-});
+function loadPage(page, storiesPerPage) {
+    getTopStoriesIDs(topStoriesUrl).then(ids => {
+        storiesFrom = (page === 1 ) ? 0 : (storiesPerPage * page);
+        storiesTo = storiesFrom + storiesPerPage - 1;
+        return loadStories(ids.slice(storiesFrom, storiesTo));
+    }).then(() => {
+        let div = document.createElement('div');
+        let p = document.createElement('p');
+        p.innerText = page;
+        div.classList.add('control');
+        const buttonPrevious = document.createElement('button');
+        const buttonNext = document.createElement('button');
+        buttonPrevious.classList.add('button_previous');
+        buttonPrevious.innerText = 'Previous';
+        buttonNext.innerText = 'Next';
+        buttonNext.classList.add('button_next');
+        div.appendChild(buttonPrevious);
+        div.appendChild(p);
+        div.appendChild(buttonNext);
+        body.appendChild(div);
+    }); 
+} 
 
-// getTopStories(topStoriesUrl);
+
+loadPage(page,10);
